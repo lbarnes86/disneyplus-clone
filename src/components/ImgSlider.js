bf216e056ea5010243bbab132000573dcdaebb17
L II <lii@Ls-MacBook-Pro.lan>
2021-11-05 16:59:27 -0500

    added Viewers.js

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -45,7 +45,7 @@ const Carousel = styled(Slider)`
         }
     }
 
-    li.slick-active button::before {
+    li.slick-active button:before {
         color: white;
     }
 
@@ -61,6 +61,7 @@ const Carousel = styled(Slider)`
 `
 
 const Wrap = styled.div`
+    cursor: pointer;
     img {
         border: 4px solid transparent;
         border-radius: 4px;
@@ -68,9 +69,11 @@ const Wrap = styled.div`
         height: 100%;
         box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
         rgb(0 0 0 / 73%) 0px 16px 10px -10px; 
+        transition-duration: 300ms;
 
         &:hover {
             border: 4px solid rgba(249, 249, 249, 0.8);
+
         }
     }
 `
