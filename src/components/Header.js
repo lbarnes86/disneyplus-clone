ca6265c9b47c5b8348060f4654c0c5456d60a822
L II <lii@Ls-MacBook-Pro.lan>
2021-11-05 15:46:41 -0500

    added navbar images

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,32 @@ import styled from 'styled-components'
 function Header() {
     return (
         <Nav>
-            <Logo src="">
+            <Logo src="/images/logo.svg" />
             <NavMenu>
-
+                <a>
+                    <img src="/images/home-icon.svg" />
+                    <span>HOME</span>
+                </a>
+                <a>
+                    <img src="/images/search-icon.svg" />
+                    <span>SEARCH</span>
+                </a>
+                <a>
+                    <img src="/images/watchlist-icon.svg" />
+                    <span>WATCHLIST</span>
+                </a>
+                <a>
+                    <img src="/images/original-icon.svg" />
+                    <span>ORIGINALS</span>
+                </a>
+                <a>
+                    <img src="/images/movie-icon.svg" />
+                    <span>MOVIE</span>
+                </a>
+                <a>
+                    <img src="/images/series-icon.svg" />
+                    <span>SERIES</span>
+                </a>
 
             </NavMenu>
         </Nav>
@@ -18,11 +41,30 @@ export default Header
 const Nav = styled.nav`
     height: 70px;
     background-color: #090b13;
+    display: flex;
+    align-items: center;
+    padding: 0 36px;
 `
 const Logo =styled.img`
     width: 80px;
 
 `
 const NavMenu = styled.div`
-    
+    display: flex;
+
+    a {
+        display: flex;
+        align-items: center;
+        padding: 0 12px;
+
+        img {
+            height: 20px;
+        }
+
+        span {
+            font-size: 13px;
+            letter-spacing: 1.42px;
+
+        }
+    }
 `
