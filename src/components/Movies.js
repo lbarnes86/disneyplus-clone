a2a79333dca3cffe71f58ab5105f4ecd4ef6c2ed
L II <lii@Ls-MacBook-Pro.lan>
2021-11-05 19:01:40 -0500

    added Detail.js

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -18,6 +18,18 @@ function Movies() {
                 <Wrap>
                     <img src="https://ntvb.tmsimg.com/assets/p18722572_b_h8_ak.jpg?w=400&aspectRatio=1.78&format=jpeg" alt="" />
                 </Wrap>
+                <Wrap>
+                    <img src="https://ntvb.tmsimg.com/assets/p18722572_b_h8_ak.jpg?w=400&aspectRatio=1.78&format=jpeg" alt="" />
+                </Wrap>
+                <Wrap>
+                    <img src="https://ntvb.tmsimg.com/assets/p18722572_b_h8_ak.jpg?w=400&aspectRatio=1.78&format=jpeg" alt="" />
+                </Wrap>
+                <Wrap>
+                    <img src="https://ntvb.tmsimg.com/assets/p18722572_b_h8_ak.jpg?w=400&aspectRatio=1.78&format=jpeg" alt="" />
+                </Wrap>
+                <Wrap>
+                    <img src="https://ntvb.tmsimg.com/assets/p18722572_b_h8_ak.jpg?w=400&aspectRatio=1.78&format=jpeg" alt="" />
+                </Wrap>
                
             </Content>
         </Container>
@@ -39,15 +51,24 @@ const Content = styled.div`
 
 const Wrap = styled.div`
     border-radius: 10px;
+    cursor: pointer;
     overflow: hidden;
     border: 3px solid rgba(249, 249, 249, 0.1);
     box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px, 
     rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
 
     img {
         width: 100%;
         height: 100%;
         object-fit: cover;
 
+    }
+
+    &:hover {
+        transform: scale(1.05);
+        box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px, 
+    rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+        border-color: rgba(249, 249, 249, 0.8);
     }
 `
